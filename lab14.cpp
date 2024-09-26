032a3798a3f73e2a0565f3c3d9bbf2705fb7bf21
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-09-25 23:08:27 -0700

    completion commit

--- lab14.cpp
+++ lab14.cpp
@@ -1,7 +1,7 @@
 // COMSC-210 | Lab 14 | Shuhaib Walahi
 
 #include <iostream>
-#include <vector>
+#include <iomanip>
 
 using namespace std; 
 
@@ -26,13 +26,44 @@ class Color
         // method for printing 
         void print() const
         {
-            cout << "RGB(" << red << ", " << green << ", " << blue << ")" << endl;
+            cout << "Red: " << setw(3) << red << ", "
+            << "Green: " << setw(3) << green << ", "
+            << "Blue: " << setw(3) << blue << endl;
         }
 };
 
 int main ()
 {
     Color firstColor, secondColor, thirdColor;
+    
+      // create color one with the setters
+    firstColor.setRed(255);
+    firstColor.setBlue(255);
+    firstColor.setGreen(255);
+
+    // create color two with the setters
+    secondColor.setRed(255);
+    secondColor.setBlue(255);
+    secondColor.setGreen(0);
+
+    // create color three with the setters
+    thirdColor.setRed(0);
+    thirdColor.setBlue(255);
+    thirdColor.setGreen(255);
+
+    // output color 1
+    cout << "Color 1: ";
+    firstColor.print();
+
+    // output color 2
+    cout << "Color 2: ";
+    secondColor.print();
+
+    // output color 3
+    cout << "Color 3: ";
+    thirdColor.print();
+
+    cout << endl;
     
     return 0; 
 }
